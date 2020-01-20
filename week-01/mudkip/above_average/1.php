e05c569b08204d41500a43da5044d871eb894f45
Mudkip <me@mudkip.pw>
2020-01-20 01:03:46 -0330

    percent sign and don't print a newline for the last line

--- week-01/mudkip/above_average/1.php
+++ week-01/mudkip/above_average/1.php
@@ -1,12 +1,12 @@
 <?php
 
 $in = fopen('php://stdin', 'r');
-$lines = fgets($in);
+$lines = (int)fgets($in);
 
 for($i = 0; $i < $lines; $i++) {
 	$g = explode(" ", fgets($in));
 	$c = array_shift($g);
 	$avg = array_sum($g)/$c;
 	$above_avg = array_filter($g, function($val) use ($avg) { return $val > $avg; });
-	printf("%.3f" . PHP_EOL, (count($above_avg)/$c)*100);
+	printf("%.3f%%" . (($i == $lines-1) ? "" : PHP_EOL), (count($above_avg)/$c)*100);
 }
