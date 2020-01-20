ec6a496eccc4c1ead33ec85776385b8bf871e882
Mudkip <me@mudkip.pw>
2020-01-20 00:29:40 -0330

    Whoops. Actual line numbers in above average

--- week-01/mudkip/above_average/1.php
+++ week-01/mudkip/above_average/1.php
@@ -3,7 +3,7 @@
 $in = fopen('php://stdin', 'r');
 $lines = fgets($in);
 
-for($i = 0; $i < 5; $i++) {
+for($i = 0; $i < $lines; $i++) {
 	$g = explode(" ", fgets($in));
 	$c = array_shift($g);
 	$avg = array_sum($g)/$c;
