2e5017f969df1e60c66360dd91ccbf21f48e8f5c
theteamaker <kingwashboard@gmail.com>
2020-01-22 01:36:07 -0330

    wewoewow

--- week-01/theteamaker/aboveaverage/target/debug/aboveaverage.d
+++
@@ -1,1 +0,0 @@
-/home/evelyn/Documents/kattis/rust/aboveaverage/target/debug/aboveaverage: /home/evelyn/Documents/kattis/rust/aboveaverage/src/main.rs
--- week-01/theteamaker/aboveaverage/target/debug/deps/aboveaverage-2453cc7489c5f90b.d
+++
@@ -1,5 +0,0 @@
-/home/evelyn/Documents/kattis/rust/aboveaverage/target/debug/deps/aboveaverage-2453cc7489c5f90b: src/main.rs
-
-/home/evelyn/Documents/kattis/rust/aboveaverage/target/debug/deps/aboveaverage-2453cc7489c5f90b.d: src/main.rs
-
-src/main.rs:
