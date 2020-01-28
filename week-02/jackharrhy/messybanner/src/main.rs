a17c2c7c7abaadb337a9bedeeff89c2fd1992f5f
Jack Arthur Harrhy <me@jackharrhy.com>
2020-01-28 13:47:01 -0330

    fixup regex, add readme

--- week-02/jackharrhy/messybanner/src/main.rs
+++ week-02/jackharrhy/messybanner/src/main.rs
@@ -3,7 +3,7 @@ use std::io::{self, BufRead};
 
 fn main() {
     let room_regex =
-        Regex::new(r"(?P<building>([A-Z])([A-Z]| ){2})(?P<room>(\d){4}([A-Z])?)($|\n)").unwrap();
+        Regex::new(r"(([A-Z]  )|([A-Z]{2} )|([A-Z]{3}))((\d){4}([A-Z])?)($|\n)").unwrap();
 
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
